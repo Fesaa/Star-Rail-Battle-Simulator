051410f4351997a9c1f5b9b460c8c7ce448fee01
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-12-03 15:10:36 +0100

    Fix Sigonia being fixed Cd

--- src/main/java/art/ameliah/hsr/relics/ornament/SigoniaTheUnclaimedDesolation.java
+++ src/main/java/art/ameliah/hsr/relics/ornament/SigoniaTheUnclaimedDesolation.java
@@ -1,9 +1,13 @@
 package art.ameliah.hsr.relics.ornament;
 
 import art.ameliah.hsr.characters.AbstractCharacter;
+import art.ameliah.hsr.characters.DamageType;
+import art.ameliah.hsr.enemies.AbstractEnemy;
 import art.ameliah.hsr.powers.PermPower;
 import art.ameliah.hsr.powers.PowerStat;
 import art.ameliah.hsr.relics.AbstractRelicSetBonus;
+
+import java.util.List;
 
 /**
  * Enemies don't actually die, so the CD boost can't be implemented.
@@ -11,26 +15,12 @@ import art.ameliah.hsr.relics.AbstractRelicSetBonus;
  */
 public class SigoniaTheUnclaimedDesolation extends AbstractRelicSetBonus {
 
-    private final float CD_BOOST;
-
     public SigoniaTheUnclaimedDesolation(AbstractCharacter<?> owner, boolean fullSet) {
-        this(owner, fullSet, 1);
+        super(owner, fullSet);
     }
 
     public SigoniaTheUnclaimedDesolation(AbstractCharacter<?> owner) {
-        this(owner, 1);
-    }
-
-    public SigoniaTheUnclaimedDesolation(AbstractCharacter<?> owner, boolean fullSet, float uptime) {
-        super(owner, fullSet);
-
-        this.CD_BOOST = Math.max(0, Math.min(1, uptime));
-    }
-
-    public SigoniaTheUnclaimedDesolation(AbstractCharacter<?> owner, float uptime) {
         super(owner);
-
-        this.CD_BOOST = Math.max(0, Math.min(1, uptime));
     }
 
     @Override
@@ -38,8 +28,21 @@ public class SigoniaTheUnclaimedDesolation extends AbstractRelicSetBonus {
         this.owner.addPower(PermPower.create(PowerStat.CRIT_CHANCE, 4, "Sigonia The Unclaimed Desolation CR Boost"));
     }
 
-    @Override
-    public void onCombatStart() {
-        this.owner.addPower(PermPower.create(PowerStat.CRIT_DAMAGE, 10 * this.CD_BOOST, "Sigonia The Unclaimed Desolation CD Boost"));
+    public static class SigoniaTheUnclaimedDesolationCDPower extends PermPower {
+        public static final String NAME = "Sigonia The Unclaimed Desolation CR Boost";
+
+        public SigoniaTheUnclaimedDesolationCDPower() {
+            super(NAME);
+        }
+
+        @Override
+        public void onEnemyRemove(AbstractEnemy enemy, int idx) {
+            this.stacks = Math.min(this.stacks+1, 10);
+        }
+
+        @Override
+        public float getConditionalCritDamage(AbstractCharacter<?> character, AbstractEnemy enemy, List<DamageType> damageTypes) {
+            return this.stacks*10;
+        }
     }
 }
