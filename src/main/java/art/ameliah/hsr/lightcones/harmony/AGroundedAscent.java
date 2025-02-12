4f5474cbee12f2b7de29eb47b755d5e85c3bf2f3
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-02-12 10:42:58 +0100

    Fix a grounded ascent

--- src/main/java/art/ameliah/hsr/lightcones/harmony/AGroundedAscent.java
+++ src/main/java/art/ameliah/hsr/lightcones/harmony/AGroundedAscent.java
@@ -3,6 +3,8 @@ package art.ameliah.hsr.lightcones.harmony;
 import art.ameliah.hsr.characters.AbstractCharacter;
 import art.ameliah.hsr.characters.DamageType;
 import art.ameliah.hsr.characters.MoveType;
+import art.ameliah.hsr.characters.Summoner;
+import art.ameliah.hsr.characters.harmony.sunday.Sunday;
 import art.ameliah.hsr.enemies.AbstractEnemy;
 import art.ameliah.hsr.lightcones.AbstractLightcone;
 import art.ameliah.hsr.powers.TempPower;
@@ -11,6 +13,9 @@ import java.util.Collection;
 import java.util.List;
 
 public class AGroundedAscent extends AbstractLightcone {
+
+    private int counter = 0;
+
     public AGroundedAscent(AbstractCharacter<?> owner) {
         super(1164, 476, 529, owner);
     }
@@ -20,14 +25,20 @@ public class AGroundedAscent extends AbstractLightcone {
         if (targets.size() != 1 || !(action.equals(MoveType.SKILL) || action.equals(MoveType.ULTIMATE))) {
             return;
         }
+        this.counter++;
+        var target = targets.stream().findFirst().get();
 
         this.owner.increaseEnergy(6, false, "A Grounded Ascent");
-        targets.stream().findFirst().ifPresent(p -> p.addPower(new Hymn()));
-
-        if (action.equals(MoveType.ULTIMATE)) {
-            if (this.owner.getActionMetric().frequency(MoveType.ULTIMATE) % 2 == 1) {
-                getBattle().generateSkillPoint(this.owner, 1);
+        target.addPower(new Hymn());
+        if (this.owner instanceof Sunday && target instanceof Summoner summoner) {
+            var summon = summoner.getSummon();
+            if (summon != null) {
+                summon.addPower(new Hymn());
             }
+        }
+
+        if (this.counter%2==0) {
+            getBattle().generateSkillPoint(this.owner, 1);
         }
     }
 
